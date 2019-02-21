f139c769e619587f4eda5e572732ad59a2659930
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2019-02-21 13:21:41 +0200

    add comment

--- src/status_im/ui/components/status_bar/view.cljs
+++ src/status_im/ui/components/status_bar/view.cljs
@@ -70,7 +70,19 @@
     :wallet-transactions-filter      {:type :modal-wallet}}
    view-id))
 
-(defn set-status-bar [view-id]
+(defn set-status-bar
+  "If more than one `StatusBar` is rendered, the one which was mounted last will
+  have higher priority
+  https://facebook.github.io/react-native/docs/statusbar.html
+
+  This means that if we have more than one screen rendered at the same time
+  (which happens due to bottom nav change), it might happen that the screen
+  which was already rendered before will be reopened but the `StatusBar` from
+  a different screen which still exists but is blurred will be applied.
+  Thus we need to set props to `StatusBar` imperatively when a particular screen
+  is shown. At the same time, the background of `StatusBar` should be rendered
+  inside the screen in order to have better transitions between screens."
+  [view-id]
   (let [{:keys [type]} (get-config view-id)
         {:keys [background-color bar-style hidden
                 network-activity-indicator-visible
